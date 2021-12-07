449661a0894deb0c6c15e87286d6d9f8df2f7669
DokuSan <92054325+DokuSan00@users.noreply.github.com>
2021-12-07 14:18:42 -0500

    created base AI

--- TDLA-base/Enemy.java
+++ TDLA-base/Enemy.java
@@ -6,52 +6,54 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Enemy extends Actor
+public class MeeleeEnemy extends Actor
 {
     private double hp;
     private double armor;
     private double dmg;
     private int type; //1 ground, 2 fly, 3 elite monster (if wanted)
-    private int hSpeed = 2;
-    private boolean inBattle;
-    private int wanderTime;
+    private char dir;
     /**
      * @param type 1 = ground, 2 = fy
      * @param level monster's lv
      */
-    public Enemy(int type, int level) // level only needed if wanted enemy be stronger each stage
+    public Enemy(int level) // level only needed if wanted enemy be stronger each stage
     {
-        hp = 100; //base hp for monster
-        switch (type) {
-            case 1:
-                 armor = 17;
-                 dmg = 10;
-                 break;
-             case 2:
-                 armor = 10;
-                 dmg = 20;
-        }
+        dir = 'r';
+        hp = 100;
+        armor = 17;
+        dmg = 10;
+        break;
     }
     
     public void act()
     {
-        
+        if (getX() - player.getX() > 0) dir = 'l';
+        else dir = 'r';
+        hSpeed = (dir == 'r') ? 10 : -10;
     }
     
     private void move() {
+        Actor player = (Actor) getWorld().getObject(Player.class).get(0);
         
+        if (isSpot() && !isAttacking())
+            setLocation(getX() + hSpeed, getY());
     }
-    private void isSpot() // detect player in range (currently, radius = 20)
+    private boolean isSpot() // detect player in range (currently, radius = 20)
     {
-        if (getObjectsInRange(0, Player.class).isEmpty()) inBattle = false;
-        else inBattle = true;
+        return getObjectsInRange(10, Player.class).isEmpty();
+    }
+    
+    private boolean isAttacking() {
+        return getObjectInRange(getImage().getWidth() / 2, Player.class);
     }
     
     private void attack() {
-        
+        if (isAttacking())
+            //  attack codes
     }
     
-    private void beHit() {
+    private void setAnimation() {
         
     }
 }
